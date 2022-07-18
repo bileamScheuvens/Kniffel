70d1f31150ef447fa3fa95c5e77e9cfe4c90058e
bileamScheuvens <benedictbileam@gmx.de>
2022-07-18 18:36:18 +0200

    Finish Environment

--- kniffel_env.jl
+++ kniffel_env.jl
@@ -2,15 +2,17 @@ using ReinforcementLearning
 using IntervalSets
 using StatsBase
 using DataStructures
+using Random
+using DataFrames
 
 """
 Define Gym
 """
 Base.@kwdef mutable struct KniffelGym <: AbstractEnv
-    dices = zeros(5)
-    fields = fill(nothing, 13)
+    dices::Vector{Int} = rand(1:6, 5)
+    fields::Vector{Union{Nothing, Int}} = fill(nothing, 13)
     # track total reward
-    reward::Float32 = 0
+    total_reward::Int = 0
 end
 
 # define action space
@@ -24,59 +26,70 @@ RLBase.legal_action_space(env::KniffelGym) = [x for x in 1:13 if check_action(x,
 # same as above just as BitArray mask
 RLBase.legal_action_space_mask(env::KniffelGym) = [check_action(x, env.dices, env.fields) for x in 1:13]
 # access state
-RLBase.state(env::KniffelGym) = nothing
+RLBase.state(env::KniffelGym) = [env.dices; env.fields]
 # access statespace
-RLBase.state_space(env::KniffelGym) = nothing
+RLBase.state_space(env::KniffelGym) = Space(18)
 # check if terminated
 RLBase.is_terminated(env::KniffelGym) = sum(isnothing.(env.fields)) == 0
+
+function RLBase.reward(env::KniffelGym)
+    filled_fields = filter(x->!isnothing(x), env.fields)
+    sum(filled_fields) + (sum([x for x in env.fields if !isnothing(x)]) >= 63 ? 35 : 0)
+end
+
 # reset gym
 function RLBase.reset!(env::KniffelGym)
-    env.dices = zeros(5)
+    env.dices = rand(1:6, 5)
     env.fields = fill(nothing, 13)
-    env.reward = 0
+    env.total_reward = reward(env)
 end
 
 """
 Take an action
 """
 function (env::KniffelGym)(action)
-    return nothing
+    env.fields[action] = check_action(action, env.dices, env.fields)
+    filled_fields = filter(x->!isnothing(x), env.fields)
+    env.total_reward = length(filled_fields) > 0 ? sum(filled_fields) : 0
+    "field $action filled with $(env.fields[action]) points"
+    state(env)
+    env.dices = rand(1:6, 5)
 end
 
-function check_action(desired::Int8, dices::Vector{Int8}, fields::Vector{Int8})
+function check_action(desired::Int, dices::Vector{Int}, fields)
+    potential_reward = eval_action(desired, dices)
     agdices = DefaultDict(0, countmap(dices))
     # field is full
     if !isnothing(fields[desired])
-        return false
+        @error "unknown or illegal action $action"
     # kniffel thrown
     elseif length(agdices) == 1 && !isnothing(fields[12])
-        return true
+        return eval_action(desired, dices)
     # 1s to 6s
     elseif desired in 1:6
-        return true
+        return eval_action(desired, dices)
     # multiples
     elseif desired in [7,8]
-        return maximum(values(agdices)) >= desired-4
+        return (maximum(values(agdices)) >= desired-4) ? potential_reward : 0
     # full house
     elseif desired == 9
-        return maximum(values(agdices)) == 3 && length(agdices) == 2
+        return (maximum(values(agdices)) == 3 && length(agdices) == 2) ? potential_reward : 0
     # streets
     elseif desired in [10,11]
         street_candidates = sort(unique([1,3,4,5,6]))
         street_candidates = join([street_candidates[i]-street_candidates[i-1] for i in 2:length(street_candidates)])
         patterns = [r"111", r"1111"]
-        return (patterns[desired-9], street_candidates)
-    # kniffel chosen
+        return occursin(patterns[desired-9], street_candidates) ? potential_reward : 0
     elseif desired == 12
-        return length(agdices) == 1
+        return (length(agdices) == 1) ? potential_reward : 0
     # chance
     elseif desired == 13
-        return true
+        return potential_reward
     end
     return "Check Kaputt"
 end
 
-function eval_action(desired::Int8, dices::Vector{Int8}, fields::Vector{Int8})
+function eval_action(desired::Int, dices::Vector{Int})
     agdices = DefaultDict(0, countmap(dices))
     # kniffel exception
     if length(agdices) == 1
@@ -97,3 +110,23 @@ function eval_action(desired::Int8, dices::Vector{Int8}, fields::Vector{Int8})
     return "Eval Kaputt"
 end
 
+function show_state(env::KniffelGym)
+    dices = env.dices
+    fields = env.fields
+    round = length(filter(x->!isnothing(x), fields)) + 1 
+    label_dice=["Dice 1", "Dice 2", "Dice 3", "Dice 4", "Dice 5"]
+    label_field = [
+        "Ones", "Twos", "Threes", "Fours", "Fives", "Sixs", "Three of a kind",
+        "Four of a kind", "Full House", "Short Street", "Long Street", "Kniffel", "Chance"
+    ]
+    println("Current Round: $round")
+    println(DataFrame(Label=label_dice, Faces=dices), "\n")
+    println(DataFrame(Label=label_field, Value=fields))
+end
+
+
+env = KniffelGym()
+env(7)
+show_state(env)
+
+filter(x->!isnothing(x), env.fields)
